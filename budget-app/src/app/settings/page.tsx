a19a40f27f7174c2117df8bc9b3b4a54a11b8047
Kevin Chau <kchau57@uwo.ca>
2024-11-29 23:51:34 -0500

    setting up pages, side bar menu

---
+++ budget-app/src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Settings() {
+    return (
+      <div>
+        <h1>Settings</h1>
+        <p>Change your profile settings here.</p>
+      </div>
+    );
+  }
+  
---
+++ budget-app/src/components/sidebar.tsx
@@ -0,0 +1,57 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "WORTH!?",
+        url: "/",
+        icon: Search,
+    },
+    {
+        title: "Dashboard",
+        url: "/dashboard",
+        icon: Home,
+    },
+    {
+        title: "Settings",
+        url: "/settings",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>WORTH?!</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
