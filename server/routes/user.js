4692c2b843e6069d28d81a4e4f031c342657eb1a
Daniel <daniel.lam3987@gmail.com>
2024-11-30 16:25:10 -0500

    Signup, login, get request for username done

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
+localhost:5000/settings
 
 
 // GET back a user based on username
-router.get('/:username', async (req, res) => {
+router.get('/api/users/:username', async (req, res) => {
 
     const { username } = req.params;
 
@@ -22,19 +24,33 @@ router.get('/:username', async (req, res) => {
 })
 
 // POST registration. Includes an email for the account
-router.post('/signup', async (req, res) => {
+router.post('/api/users/signup', async (req, res) => {
     const { username, password, email} = req.body;
+    if (!username || !email || !password || username.length === 0 || email.length === 0 || password.length === 0) {
+        return res.status(400).send('Username, email, and password cannot be empty');
+    }
     try{
-        const result = await req.db.collection('Users').insertOne({username, password, email})
-        res.status(201).json({insertedId: result.insertedId})
+        const existingUser = await req.db.collection('Users').findOne({ username })
+        if(existingUser){
+            res.status(409).send("Existing username already exists")
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await req.db.collection('Users').insertOne({ 
+            username, 
+            password: hashedPassword, 
+            email 
+        });
+
+        res.status(201).json({ insertedId: result.insertedId });
     }
     catch{
         res.status(500).send('Error creating user.')
     }
 })
 
-// POST. Uses Bycrypt for security purposes
-router.post('/login', async (req, res) => {
+// POST. Login section. Use username and password. Responds back with which one is wrong.
+router.post('/api/users/login', async (req, res) => {
     const { username, password } = req.body;
 
     try{
@@ -42,9 +58,11 @@ router.post('/login', async (req, res) => {
         if (!user){
             return res.status(404).send("Error user not found")
         }
-        if(password != user.password){
-            res.status(401).send("Error password is invalid")
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).send('Invalid credentials');
         }
+
         
         res.status(200).send('Login Success')
     }
@@ -52,6 +70,5 @@ router.post('/login', async (req, res) => {
         res.status(500).send('Error creating user.')
     }
 })
-
 
 module.exports = router
