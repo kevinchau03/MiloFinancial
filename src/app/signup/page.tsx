3aea2a7f7ab916d8906923dfcb0ad012af0678b8
Kevin Chau <kchau57@uwo.ca>
2024-12-17 11:03:22 -0500

    sign up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,9 +11,9 @@ export default function Register() {
   const ref = useRef<HTMLFormElement>(null);
   const handleSubmit = async (formData: FormData) => {
     const r = await register({
-        email: formData.get("email"),
-        password: formData.get("password"),
-        name: formData.get("name")    
+        email: formData.get("email") as string || "",
+        password: formData.get("password") as string || "",
+        name: formData.get("name") as string || ""
       });
       ref.current?.reset();
       if(r?.error){
